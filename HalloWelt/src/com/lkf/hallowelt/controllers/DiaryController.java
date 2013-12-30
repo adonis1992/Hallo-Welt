b9d9e7623de640a9370fed24b96843c70fc74849
adonis1992 <627917316@qq.com>
2013-12-30 18:19:49 +0800

    Adonis: photochoose complete.

--- HalloWelt/src/com/lkf/hallowelt/controllers/DiaryController.java
+++ HalloWelt/src/com/lkf/hallowelt/controllers/DiaryController.java
@@ -1,15 +1,62 @@
 package com.lkf.hallowelt.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.os.Bundle;
+
+import com.lkf.hallowelt.helpers.ResourceHelper;
+import com.lkf.hallowelt.screens.diary.BookCaseView;
+import com.lkf.hallowelt.screens.diary.DiaryView;
+import com.lkf.hallowelt.screens.diary.PhotoChooseView;
 import com.lkf.lib.base.LKFScreen;
 
 public class DiaryController extends ModelController
-{
-
+{	
+	//Screens
+	private BookCaseView theBookCaseView;
+	private DiaryView theDiaryView;
+	private PhotoChooseView thePhotoChooseView;
+	
+	private int diaryID;
+	private int currentPage;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		addBaseView();
+	}
+	
 	@Override
 	public LKFScreen getStartScreen()
 	{
 		// TODO Auto-generated method stub
-		return null;
+		theBookCaseView = new BookCaseView(this, 360, 640, ResourceHelper.COORDINATE_HELPER.focalLenth);
+		theDiaryView = new DiaryView(this, 360, 640, ResourceHelper.COORDINATE_HELPER.focalLenth);
+		thePhotoChooseView = new PhotoChooseView(this, 360, 640, ResourceHelper.COORDINATE_HELPER.focalLenth);
+		return theBookCaseView;
 	}
 
+	public InputStream readPhoto(String fileName) throws IOException
+	{
+		return readFile("photos" + File.separator + fileName);
+	}
+	
+	public int getDiaryID()
+	{
+		return diaryID;
+	}
+	
+	public int getCurrentPage()
+	{
+		return currentPage;
+	}
+	
+	public void getToDiary()
+	{
+		setScreen(theDiaryView);
+	}
 }
--- HalloWelt/src/com/lkf/hallowelt/screens/diary/PhotoChooseView.java
+++ HalloWelt/src/com/lkf/hallowelt/screens/diary/PhotoChooseView.java
@@ -1,12 +1,19 @@
 package com.lkf.hallowelt.screens.diary;
 
+import java.io.IOException;
+
+import android.database.Cursor;
+import android.view.KeyEvent;
+
 import com.lkf.hallowelt.controllers.DiaryController;
 import com.lkf.lib.base.LKFController;
 import com.lkf.lib.base.LKFScreen;
 import com.lkf.lib.base.framework.InputBase.LKFKeyEvent;
+import com.lkf.lib.base.framework.InputBase.LKFKeyEvent.KeyState;
 import com.lkf.lib.helpers.FingerHelper;
 import com.lkf.lib.render.Sprite;
 import com.lkf.lib.render.SpriteBatcher;
+import com.lkf.lib.render.Texture;
 
 public class PhotoChooseView extends LKFScreen
 {
@@ -14,24 +21,53 @@ public class PhotoChooseView extends LKFScreen
 	private final DiaryController theController;
 	private SpriteBatcher theBatcher;
 	
+	//Pictures
+	private Texture thePicture;
+	private Texture theComponentsAtlas;
+	
 	//Communicate Members
 	private Sprite forward;
 	private Sprite backward;
 	private Sprite confirm;
 	
+	private boolean canForward;
+	private boolean canBackward;
+	
+	//data
+	private Cursor photos;
+	private int pageNumber;
+	private int current;
 	
 	public PhotoChooseView(DiaryController controller, float width, float height, float focalLength)
 	{
 		super(width, height, focalLength);
 		// TODO Auto-generated constructor stub
 		theController = controller;
+		theBatcher = new SpriteBatcher(5, controller);
 	}
 
 	@Override
 	public void present(float deltaTime)
 	{
 		// TODO Auto-generated method stub
-
+		matrixInit();
+		
+		theController.textureRenderInit();
+		theBatcher.beginBatch(thePicture);
+		theBatcher.drawBackground();
+		theBatcher.endBatch();
+		alphaRenderInit();
+		theBatcher.beginBatch(theComponentsAtlas);
+		if (canForward)
+		{
+			theBatcher.drawSprite(forward);
+		}
+		if (canBackward)
+		{
+			theBatcher.drawSprite(backward);
+		}
+		theBatcher.drawSprite(confirm);
+		theBatcher.endBatch();
 	}
 
 	@Override
@@ -66,21 +102,82 @@ public class PhotoChooseView extends LKFScreen
 	protected void touchUpExecute(FingerHelper finger)
 	{
 		// TODO Auto-generated method stub
-
+		if (canForward && forward.touchCheck(finger.getPosition()))
+		{
+			photos.moveToPrevious();
+			setPhoto();
+		}
+		else if (canBackward && backward.touchCheck(finger.getPosition()))
+		{
+			photos.moveToNext();
+			setPhoto();
+		}
+		else if (confirm.touchCheck(finger.getPosition()))
+		{
+			theController.getDatabaseHelper().insertPhoto(photos.getInt(0), theController.getDiaryID(), theController.getCurrentPage());
+			theController.getToDiary();
+		}
 	}
 
 	@Override
 	protected void keyPressExecute(LKFKeyEvent event)
 	{
 		// TODO Auto-generated method stub
-
+		if (event.type == KeyState.KEY_UP)
+		{
+			switch (event.keyCode)
+			{
+			case KeyEvent.KEYCODE_BACK:
+			{
+				theController.getToDiary();
+				break;
+			}
+			default:
+				break;
+			}
+		}
 	}
 
 	@Override
 	protected void textureLoad()
 	{
 		// TODO Auto-generated method stub
-
+		photos = theController.getDatabaseHelper().getPhotos();
+		pageNumber = photos.getCount();
+		if (pageNumber > 0)//must have at least one picture
+		{
+			photos.moveToFirst();
+			current = 1;
+			setPhoto(); 
+		}
+	}
+	
+	private void setPhoto()
+	{
+		try
+		{
+			thePicture = new Texture(theController.readPhoto(photos.getString(1) + ".png"));
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (current == 1)
+		{
+			canForward = false;
+		}
+		else 
+		{
+			canForward = true;
+		}
+		if (current < pageNumber)
+		{
+			canBackward = true;
+		}
+		else 
+		{
+			canBackward = false;
+		}
 	}
 
 	@Override
